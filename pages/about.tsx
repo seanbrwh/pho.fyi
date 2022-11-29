0c97aa4c370d745b184d2d411de8064f06711d66
Sean White <seanbrwh@gmail.com>
2022-11-28 20:26:12 -0700

    added active class to navlinks

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,36 @@ const About: NextPageWithLayout = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen">
       <Head>
-        <title>About</title>
+        <title>Pho Moon Art Creations | About</title>
+        <meta
+          name="description"
+          content="A website to showcase art as well as sell it"
+        />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="shortcut icon"
+          href="/favicon/favicon.ico"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
       <h1>About</h1>
     </div>
--- pages/shop.tsx
+++ pages/shop.tsx
@@ -7,8 +7,38 @@ const Shop: NextPageWithLayout = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen">
       <Head>
-        <title>Shop</title>
+        <title>Pho Moon Art Creations | Shop</title>
+        <meta
+          name="description"
+          content="A website to showcase art as well as sell it"
+        />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="shortcut icon"
+          href="/favicon/favicon.ico"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
+
       <h1>Shop</h1>
     </div>
   );
