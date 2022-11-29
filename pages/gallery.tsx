e0b101a85969abec61a48acf0a6ca8c489a1ca46
Sean White <seanbrwh@gmail.com>
2022-11-28 22:23:43 -0700

    finished 3 types of carousel

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,8 +1,11 @@
 import type { ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 import Layout from "../components/layout";
-import Carousel, { CarouselItem } from "../components/carousel";
+import Carousel from "../components/carousel/carousel";
+import InfiniteCarousel from "../components/carousel/infiniteCarousel";
+import { CarouselItem } from "../components/carousel/carouselItem";
 import Head from "next/head";
+import SwipeableCarousel from "../components/carousel/swipeableCarousel";
 
 const Gallery: NextPageWithLayout = () => {
   return (
@@ -41,17 +44,45 @@ const Gallery: NextPageWithLayout = () => {
       </Head>
       <div className="w-96 h-96 flex flex-col justify-center text-center">
         <h1 className="py-5">Gallery</h1>
-        <Carousel>
-          <CarouselItem>
-            <div>Item 1</div>
-          </CarouselItem>
-          <CarouselItem>
-            <div>Item 2</div>
-          </CarouselItem>
-          <CarouselItem>
-            <div>Item 3</div>
-          </CarouselItem>
-        </Carousel>
+        <div>
+          <Carousel>
+            <CarouselItem>
+              <div>Item 1</div>
+            </CarouselItem>
+            <CarouselItem>
+              <div>Item 2</div>
+            </CarouselItem>
+            <CarouselItem>
+              <div>Item 3</div>
+            </CarouselItem>
+          </Carousel>
+        </div>
+        <div>
+          <InfiniteCarousel>
+            <CarouselItem>
+              <div>Item 4</div>
+            </CarouselItem>
+            <CarouselItem>
+              <div>Item 5</div>
+            </CarouselItem>
+            <CarouselItem>
+              <div>Item 6</div>
+            </CarouselItem>
+          </InfiniteCarousel>
+        </div>
+        <div className="py-5">
+          <SwipeableCarousel>
+            <CarouselItem>
+              <div>Item 7</div>
+            </CarouselItem>
+            <CarouselItem>
+              <div>Item 8</div>
+            </CarouselItem>
+            <CarouselItem>
+              <div>Item 9</div>
+            </CarouselItem>
+          </SwipeableCarousel>
+        </div>
       </div>
     </div>
   );
