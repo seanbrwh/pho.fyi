f88a9bc9baeefecc22eed613cf3dd1273296ee2b
Sean White <26911526+seanbrwh@users.noreply.github.com>
2022-12-01 14:51:53 -0700

    db working created user table

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,31 @@
-CREATE USER seanbrwh;
+--TODO need to create user table 
 
-CREATE SERVER pho.fyi FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'Sean_workstatio', dbname 'pho.fyi', port '5432');
+CREATE TABLE IF NOT EXISTS users(
+  user_id serial PRIMARY KEY,
+  username VARCHAR (50) NOT NULL,
+  password varchar (50) NOT NULL,
+  email VARCHAR (255) UNIQUE NOT NULL,
+  created_on TIMESTAMP NOT NULL,
+  last_login TIMESTAMP
+);
 
-GRANT ALL PRIVILEGES ON DATABASE pho.fyi TO seanbrwh;
+CREATE TABLE roles(
+  role_id serial PRIMARY KEY,
+  role_name varchar(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE user_roles(
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+  grant_date TIMESTAMP,
+  PRIMARY KEY (user_id, role_id),
+  FOREIGN KEY (role_id)
+    REFERENCES roles (role_id),
+  FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+);
+
+--TODO need to create product table
+
+--TODO need to create a blog table
+
